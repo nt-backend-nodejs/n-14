b7cf0943b85959ac5c1d175d65580c92cd7ffe14
khamidullo <khkhamidullo@gmail.com>
2024-09-13 19:16:26 +0500

    lesson 1

---
+++ main.js
@@ -0,0 +1,58 @@
+console.log("hello from main.js")
+// // salom 
+
+//     /*
+//       salom
+//     */
+
+//     // var, let, cont 
+
+//     /*
+//       var name = "azizbek"
+  
+//       name = 123
+//       console.log(name)
+// */
+//     //Number
+//     // 1, 2, -1, -1234546, 0
+//     // 1.2, 1.3
+//     // 2e10
+//     var mix = 10
+//     var max = 10
+//     var sum = mix + max
+//     console.log("sum = ", sum)
+//     console.log("sum = " + sum)
+
+//     console.info("this is data")
+
+//     console.warn("Xatolik")
+
+//     console.error("Error")
+//     // console.log("age = ", age)
+//     //string
+
+
+//     var a = "salom dunyo"
+//     console.log("a = ", a)
+
+//     var text = `Najot ta'lim 
+//     sasa  sasasa
+//     sasa
+//     `
+//     var name = ' sasa sasasa'
+
+//     // boolean
+//     // true va false
+//     var isMerried = false
+//     var isStudent = true
+
+//     var size // undefined
+//     console.log(size)
+
+//     // null va undefined
+
+//     // var age = 12
+
+
+
+let a = "salom"
