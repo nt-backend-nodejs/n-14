3a2da0c5f2c38642b1195911ddb5bf585d2a215c
khamidullo <khkhamidullo@gmail.com>
2024-11-04 22:35:16 +0500

    js objet and array

--- array.js
+++
@@ -1,8 +0,0 @@
-// const lang = "Hello javascript"
-
-// console.log(lang.replace("Hello","Assalomu alaykum!"))
-
-// console.log(lang.length)
-// console.log(lang.toUpperCase())
-
-// console.log(lang)
--- index.js
+++ index.js
@@ -1,84 +1,121 @@
-// greet("Ozodbek")
-
-// function greet(name) {
-//   console.log(`hello, ${name}`)
-// }
-
-// l("Azizbek")
-// let l = function(name){
-//   console.log(`hello, ${name}`)
-// }
-
-// l("Bexruz")
-
-// let sumOfNumber = function (str) {
-//   let sum = 0;
-//   let length = str.length;
-//   for (let i = 0; i < length; i++) {
-//     if (!isNaN(str[i])) {
-//       sum += Number(str[i]);
-//     }
-//   }
-//   return sum
+// const add = (a, b, c) => {
+//   c(a + b);
 // };
 
-// const result = sumOfNumber("sadsfdg12343sas1234s"); //=> 1+2+3+4+4+1+2+3+4
+// const logger = (num) => {
+//   console.log(num);
+// };
 
-// console.log({result})
-let a = 12;
+// add(1, 3, logger);
 
-// (function () {
-//   console.log("OKSASA");
-// })();
+// const ages1 = [1, 2, 3, 4, 5, 7, 44, 8, 293, 3920];
+// const ages2 = [1, 2, 3, 4, 5, 7, 44, 8, 293, 3920];
+// const ages3 = [1, 2, 3, 4, 5, 7, 44, 8, 293, 3920];
+// const ages4 = [1, 2, 3, 4, 5, 7, 44, 8, 293, 3920];
 
+// const newArray = ages1.concat(ages2, ages3, ages4)
+// const newArray = [...ages2, ...ages3, ...ages4, ...ages1].sort((a, b) => a - b);
+// console.log(newArray);
 
+// const newAges = ages.sort((a, b) => b - a);
 
-let greet = (name)=>{
-  console.log(`Hello, ${name}`)
-}
-// greet("Og'abek")
+// console.log(ages);
+// console.log(newAges);
 
+// console.time("inclues")
+// if (ages.includes(44)) {
+//   console.log("SIZ KATTASIZ");
+// }
+// console.timeEnd("inclues")
 
+// 1. for (let``)
+// 2. for (in)
+// 3. for (of)
+// 3. array.forEach
 
+// Array.prototype.myForEach = function (callback) {
+//   const arr = this;
+//   const length = arr.length;
 
-// let add = function(a,b){
-//   return a+b
+//   for (let i = 0; i < length; i++) {
+//     callback(arr[i])
+//   }
+// };
+
+// ages.myForEach((age) => {
+//   console.log(age);
+// });
+
+// ages.forEach((age) => {
+//   console.log(age);
+// });
+
+// const ages1 = [1, 2, 3, 4, 5, 7, 44, 8, 293, 3920];
+
+// // console.log(ages1.join(", "))
+// console.log(ages1.reverse())
+
+// const person = {
+//   firstName: "azizbek",
+//   lastName: "Bahromjonov",
+//   age: 15,
+//   isMerried: false,
+//   isStudent: false,
+//   phoneNumber: 998944040239,
+//   address: "TOSHkent",
+// };
+// console.log(person.age);
+// console.log(person["age"]);
+// const keys = Object.keys(person)
+// const values = Object.values(person)
+// const entries = Object.entries(person)
+
+// console.log(keys);
+// console.log(values);
+// console.log(entries);
+
+// const person = {
+//   firstName: "azizbek",
+//   lastName: "Bahromjonov",
+//   age: 15,
+//   isMerried: false,
+//   isStudent: false,
+//   phoneNumber: 998944040239,
+//   address: "TOSHkent",
+// };
+
+// const keys = Object.keys(person);
+
+// keys.forEach((key) => {
+//   console.log(person[key]);
+// });
+// const values = Object.values(person);
+
+// values.forEach((value) => {
+//   console.log(value);
+// });
+
+// for (const b of values) {
+//   console.log(b);
 // }
 
-// let add = (a,b)=> a + b
+// for (const index in values) {
+//   console.log(values[index]);
+// }
 
-// console.log(add(1,2))
-// let result = add(1,2)
+const contact = {
+  contacts: [],
+  add(name, contact) {
+    this.contacts.push({ name, contact });
+  },
+  getOne(name) {},
+  getAll() {
+    return this.contacts;
+  },
+  deleteOne(name) {},
+};
 
-// console.log({result})
+contact.add("Azizbek", 944040239);
+contact.add("Bexruz", 999900919);
 
-
-let reverseStr= (str)=>{
-  let length = str.length
-  let result = '';
-  for (let i = length - 1 ; i >= 0; i--) {
-    result += str[i]
-  }
-  console.log(result)
-} 
-
-reverseStr('hello')
-
-
-
-
-let  processUserInput =(test, name)=>{
-  test(name)
-}
-
-
-let b = (name)=>{
-  console.log(`Hello, ${name}`)
-}
-
-
-processUserInput(b, "Azamat")
-
-
-car["make"]
-car.starEngine()
+console.log(contact.getAll());
