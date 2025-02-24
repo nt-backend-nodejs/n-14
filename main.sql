c3895fe4432239cf50dd2bf0bf3913f35b068fc5
khamidullo <khkhamidullo@gmail.com>
2025-02-24 18:51:07 +0500

    tranzaction

--- main.sql
+++ main.sql
@@ -1,16 +1,18 @@
 --  create
 --  \! clear
 --  \l  hamma malumotlar bazasini ko'rish
--- CREATE Table "user" (
---   id SERIAL PRIMARY KEY,
---   name VARCHAR(50),
---   age INTEGER,
---   email VARCHAR(50) UNIQUE
--- );
+CREATE Table users (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(50) not null check (name <> ''),
+    birth_date Date default now (),
+    age INT CHECK (age > 0),
+    email VARCHAR(50) UNIQUE not null
+);
+
 INSERT INTO
-    "user" (name, age, email)
+    users (name, age, birth_date, email)
 VALUES
-    ('Usmon', 19, 'Usmon@gmail.com') RETURning *;
+    ('Usmon', 19, '2000-12-12', 'Usmon@gmail.com') RETURning *;
 
 SELECT
     *
@@ -31,3 +33,35 @@ select
 from
     customer
     inner join orders on customer.id = orders.order_id;
+
+-- index
+CREATE INDEX idx_name ON users (name);
+
+-- --
+-- select
+--     *
+-- from
+--     "user"
+-- where
+--     salary >= 32406.571428571428
+SELECT
+    *
+FROM
+    company_regions
+WHERE
+    region_id = (
+        select
+            region_id
+        from
+            staff
+        where
+            salary >= (
+                SELECT
+                    MAX(SALARY)
+                FROM
+                    staff
+            )
+    );
+
+--
+CREATE TYPE USER_ROLE AS ENUM ('ADMIN', 'USER', 'GUEST');
