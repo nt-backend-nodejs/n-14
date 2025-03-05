ff3e7eac4efcde30e501027434ff14ca128bc0b5
khamidullo <khkhamidullo@gmail.com>
2025-03-05 17:28:23 +0500

    update

--- src/middleware/error.middleware.js
+++
@@ -1,9 +0,0 @@
-import z from "zod";
-
-export const signUpSchema = z.object({
-	firstName: z.string(),
-	lastName: z.string(),
-	email: z.string().email(),
-	phone: z.string().phone(),
-	address: z.string(),
-});
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import { authController } from "../controllers/index.js";
+import { validateData } from "../middlewares/validationMiddleware.js";
+import { signUpSchema } from "../validations/auth.validation.js";
 
 export const authRouter = Router();
 
-authRouter.post("/signup", authController.signup);
+authRouter.post("/signup", validateData(signUpSchema), authController.signup);
 authRouter.post("/signin", authController.signin);
 authRouter.get("/profile", authController.profile);
