738b913c49edd3f6dcf8e677642b80853764af0b
khamidullo <khkhamidullo@gmail.com>
2025-03-17 19:09:24 +0500

    project file sttucture

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,22 +1,2 @@
-import { config as dotConfig } from "dotenv";
-
-dotConfig();
-
-export const config = {
-  app: {
-    port: process.env.PORT,
-  },
-  db: {
-    url: process.env.DATABASE_URL,
-  },
-  jwt: {
-    access: {
-      secret: process.env.JWT_ACCESS_SECRET,
-      expiresIn: process.env.JWT_ACCESS_EXPIRES_IN,
-    },
-    refresh: {
-      secret: process.env.JWT_REFRESH_SECRET,
-      expiresIn: process.env.JWT_REFRESH_EXPIRES_IN,
-    },
-  },
-};
+export * from "./db";
+export * from "./config";
--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,15 @@
 import express from "express";
+import { config } from "./config";
+import { setUpRouter } from "./routers";
 
 const app = express();
 
-const PORT = process.env.PORT || 4000;
+const PORT = config.app.port || 4000;
 
 app.use(express.json());
+
+//ROUTER
+app.use("/setup", setUpRouter);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
