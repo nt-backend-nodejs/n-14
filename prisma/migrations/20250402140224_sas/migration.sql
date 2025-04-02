eee2c2f18a367544fd06d08a8046d4c1e86d8ba8
khamidullo <khkhamidullo@gmail.com>
2025-04-02 19:04:57 +0500

    prisma

---
+++ prisma/migrations/20250402140224_sas/migration.sql
@@ -0,0 +1,23 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "name" TEXT NOT NULL,
+    "username" TEXT NOT NULL,
+    "password" TEXT NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "Post" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "title" TEXT NOT NULL,
+    "content" TEXT,
+    "published" BOOLEAN NOT NULL DEFAULT false,
+    "authorId" INTEGER NOT NULL,
+    CONSTRAINT "Post_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "User" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
+
+-- CreateIndex
+CREATE INDEX "User_name_password_idx" ON "User"("name", "password");
---
+++ src/script.ts
@@ -0,0 +1,36 @@
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+async function main() {
+  const user = await prisma.user.create({
+    data: {
+      name: 'Bob',
+      username: 'bob@prisma.io',
+      password: 'qwqwqw',
+      Post: {
+        create: [
+          {
+            title: 'Hello World',
+            published: true,
+          },
+          {
+            title: 'My second post',
+            content: 'This is still a draft',
+          },
+        ],
+      },
+    },
+  });
+  console.log(user);
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
